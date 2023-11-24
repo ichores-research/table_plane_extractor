e93b87036c783ad3e3a7e18eb0bcf7edde599474
Haberger David <david.haberger@tuwien.ac.at>
2023-11-24 09:25:39 +0000

    use RvisVisualizer from v4r.utils

--- src/table_plane_extractor_srv.py
+++ src/table_plane_extractor_srv.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-from math import atan, pi
 from table_plane_extractor.srv import TablePlaneExtractor, TablePlaneExtractorResponse
 from table_plane_extractor.msg import Plane
 from extractor_of_table_planes import extract_table_planes_from_pcd
-import numpy as np
 import rospy
-import open3d as o3d
 from open3d_ros_helper import open3d_ros_helper as orh
 import tf2_ros
-from v4r_util.util import o3d_bb_to_ros_bb, transformPointCloud, get_minimum_oriented_bounding_box, ros_bb_arr_to_rviz_marker_arr
+from v4r_util.util import o3d_bb_to_ros_bb, transformPointCloud
+from v4r_util.rviz_visualizer import RvizVisualizer
 from vision_msgs.msg import BoundingBox3DArray
-from visualization_msgs.msg import MarkerArray
 
 from std_msgs.msg import Header
 
@@ -26,28 +23,23 @@ def table_plane_extractor_methode(req):
 
     table_params = rospy.get_param("table_plane_extractor")
 
-    if table_params['enable_rviz_visualization']:
-        pub = rospy.Publisher('PlaneBoundingBoxVisualizer',
-                              MarkerArray, queue_size=10)
+    if table_params["enable_rviz_visualization"]:
+        rviz_vis = RvizVisualizer('TablePlaneExtractorVisualizer') 
 
     tf_buffer = tf2_ros.Buffer()
     tf2_ros.TransformListener(tf_buffer)
 
     # get pointcloud and convert from sensor_msgs/Pointcloud2 to open3d.geometry.PointCloud
     pcd = req.point_cloud
-    pcd = transformPointCloud(pcd, table_params['base_frame'], pcd.header.frame_id, tf_buffer) #make sure pointcloud has z pointing up
+
+    #make sure pointcloud has z pointing up
+    pcd = transformPointCloud(
+        pcd, table_params['base_frame'], pcd.header.frame_id, tf_buffer) 
     header = pcd.header
     pcd = orh.rospc_to_o3dpc(pcd, remove_nans=True)
 
     # downsample cloud
-    downsample_vox_size = rospy.get_param(
-        "/table_plane_extractor/downsample_vox_size")
-    pcd = pcd.voxel_down_sample(voxel_size=downsample_vox_size)
-
-    bb_arr = BoundingBox3DArray()
-    bb_arr.header = header
-
-
+    pcd = pcd.voxel_down_sample(voxel_size=table_params['downsample_vox_size'])
 
     planes, bboxes = extract_table_planes_from_pcd(
         pcd, 
@@ -58,11 +50,14 @@ def table_plane_extractor_methode(req):
         table_params["z_min"])
     
     planes_ros = [Plane(Header(0, header.stamp, table_params['base_frame']), a, b, c, d) for a, b, c, d in planes]
+
+    bb_arr = BoundingBox3DArray()
+    bb_arr.header = header
     bb_arr.boxes = [o3d_bb_to_ros_bb(bb_plane) for bb_plane in bboxes]
 
     if table_params['enable_rviz_visualization']:
-        marker_arr = ros_bb_arr_to_rviz_marker_arr(bb_arr)
-        pub.publish(marker_arr)
+        rviz_vis.publish_ros_bb_arr(bb_arr, "planes", True)
+    
     return TablePlaneExtractorResponse(planes_ros, bb_arr)
 
 
@@ -72,8 +67,10 @@ def table_plane_extractor_server():
     Topic: /test/table_plane_extractor
     '''
     rospy.init_node('table_plane_extractor_server')
-    s = rospy.Service('/test/table_plane_extractor',
-                      TablePlaneExtractor, table_plane_extractor_methode)
+    s = rospy.Service(
+        '/test/table_plane_extractor',
+        TablePlaneExtractor, 
+        table_plane_extractor_methode)
     print("Ready to extract planes")
     rospy.spin()
 

ee210184ad7ed10901a793c1bf5e16a1da840997
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2024-05-09 17:23:23 +0000

    Make tf errors go away by letting buffer fill up before its needed

--- src/table_plane_extractor_srv.py
+++ src/table_plane_extractor_srv.py
@@ -12,80 +12,81 @@ from vision_msgs.msg import BoundingBox3DArray
 from std_msgs.msg import Header
 
 
-
-def table_plane_extractor_methode(req):
-    ''' 
-    Table plane extractor who takes the point cloud as input. Returns possible horizontal planes 
-    with plane equation (x, y, z, d -> a * x + b * y + c * z + d = 0) and bounding boxes around the planes.
-    Input: sensor_msgs/PointCloud2 inputCloud
-    Output: table_plane_extractor/Plane[] planes, vision_msgs/BoundingBox3DArray plane_bounding_boxes
-    '''
-
-    table_params = rospy.get_param("table_plane_extractor")
-
-    if table_params["enable_rviz_visualization"]:
-        rviz_vis = RvizVisualizer('TablePlaneExtractorVisualizer') 
-
-    tf_buffer = tf2_ros.Buffer()
-    tf2_ros.TransformListener(tf_buffer)
-
-    # get pointcloud and convert from sensor_msgs/Pointcloud2 to open3d.geometry.PointCloud
-    pcd = req.point_cloud
-
-    #make sure pointcloud has z pointing up
-    pcd = transformPointCloud(
-        pcd, 
-        table_params['base_frame'], 
-        pcd.header.frame_id, 
-        tf_buffer) 
-    header = pcd.header
-    pcd = orh.rospc_to_o3dpc(pcd, remove_nans=True)
-
-    # downsample cloud
-    pcd = pcd.voxel_down_sample(voxel_size=table_params['downsample_vox_size'])
-
-    planes, bboxes = extract_table_planes_from_pcd(
-        pcd, 
-        table_params["cluster_dbscan_eps"], 
-        table_params["min_cluster_size"], 
-        table_params["plane_segmentation_distance_threshold"], 
-        table_params["max_angle_deg"], 
-        table_params["z_min"],
-        table_params["num_iter_ransac"],
-        table_params["min_pre_cluster_size"])
+class TablePlaneExtractorServer():
     
-    if planes is None:
-        rospy.logerr("No planes found!")
-        return None, None
-    
-    planes_ros = [
-        Plane(
-            Header(0, header.stamp, table_params['base_frame']), 
-            a, b, c, d) for a, b, c, d in planes]
+    def __init__(self):
+        ''' 
+        Starting table plane extractor server node
+        Topic: /table_plane_extractor/get_planes
+        '''
+        rospy.init_node('table_plane_extractor_server')
+        s = rospy.Service(
+            '/table_plane_extractor/get_planes',
+            TablePlaneExtractor, 
+            self.table_plane_extractor_methode)
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tl = tf2_ros.TransformListener(self.tf_buffer)
+        print("Ready to extract planes")
+        
 
-    bb_arr = BoundingBox3DArray()
-    bb_arr.header = header
-    bb_arr.boxes = [o3d_bb_to_ros_bb(bb_plane) for bb_plane in bboxes]
+    def table_plane_extractor_methode(self, req):
+        ''' 
+        Table plane extractor who takes the point cloud as input. Returns possible horizontal planes 
+        with plane equation (x, y, z, d -> a * x + b * y + c * z + d = 0) and bounding boxes around the planes.
+        Input: sensor_msgs/PointCloud2 inputCloud
+        Output: table_plane_extractor/Plane[] planes, vision_msgs/BoundingBox3DArray plane_bounding_boxes
+        '''
 
-    if table_params['enable_rviz_visualization']:
-        rviz_vis.publish_ros_bb_arr(bb_arr, "table_plane", True)
-    
-    return TablePlaneExtractorResponse(planes_ros, bb_arr)
+        table_params = rospy.get_param("table_plane_extractor")
 
+        if table_params["enable_rviz_visualization"]:
+            rviz_vis = RvizVisualizer('TablePlaneExtractorVisualizer') 
 
-def table_plane_extractor_server():
-    ''' 
-    Starting table plane extractor server node
-    Topic: /table_plane_extractor/get_planes
-    '''
-    rospy.init_node('table_plane_extractor_server')
-    s = rospy.Service(
-        '/table_plane_extractor/get_planes',
-        TablePlaneExtractor, 
-        table_plane_extractor_methode)
-    print("Ready to extract planes")
-    rospy.spin()
+        
 
+        # get pointcloud and convert from sensor_msgs/Pointcloud2 to open3d.geometry.PointCloud
+        pcd = req.point_cloud
+
+        #make sure pointcloud has z pointing up
+        pcd = transformPointCloud(
+            pcd, 
+            table_params['base_frame'], 
+            pcd.header.frame_id, 
+            self.tf_buffer) 
+        header = pcd.header
+        pcd = orh.rospc_to_o3dpc(pcd, remove_nans=True)
+
+        # downsample cloud
+        pcd = pcd.voxel_down_sample(voxel_size=table_params['downsample_vox_size'])
+
+        planes, bboxes = extract_table_planes_from_pcd(
+            pcd, 
+            table_params["cluster_dbscan_eps"], 
+            table_params["min_cluster_size"], 
+            table_params["plane_segmentation_distance_threshold"], 
+            table_params["max_angle_deg"], 
+            table_params["z_min"],
+            table_params["num_iter_ransac"],
+            table_params["min_pre_cluster_size"])
+        
+        if planes is None:
+            rospy.logerr("No planes found!")
+            return None, None
+        
+        planes_ros = [
+            Plane(
+                Header(0, header.stamp, table_params['base_frame']), 
+                a, b, c, d) for a, b, c, d in planes]
+
+        bb_arr = BoundingBox3DArray()
+        bb_arr.header = header
+        bb_arr.boxes = [o3d_bb_to_ros_bb(bb_plane) for bb_plane in bboxes]
+
+        if table_params['enable_rviz_visualization']:
+            rviz_vis.publish_ros_bb_arr(bb_arr, "table_plane", True)
+        
+        return TablePlaneExtractorResponse(planes_ros, bb_arr)
 
 if __name__ == "__main__":
-    table_plane_extractor_server()
+    srv = TablePlaneExtractorServer()
+    rospy.spin()
